827b703922fd3f06fba350829c253ade86028e3f
Jerome V. <jerome@euzeo.com>
2012-10-31 17:33:54 +0100

    converted space to tab...

--- Command/FixtureCommand.php
+++ Command/FixtureCommand.php
@@ -13,104 +13,113 @@ use Symfony\Component\Yaml\Yaml;
 class FixtureCommand extends ContainerAwareCommand
 {
 	/**
-	 * EntityManager
+	 * @var EntityManager
 	 */
 	protected $em;
+	
+	/**
+	 * @var array In this array we'll store all entity objects
+	 */
+	protected $entities;
+	
+	/**
+	 * @var string
+	 */
+	protected $bundle;
 
-    protected function configure()
-    {
-        $this
-            ->setName('euzeo:fixtures')
-            ->setDescription('Load db data from fixtures files')
-            ->addArgument('bundle', InputArgument::OPTIONAL, 'The bundle name')
-            ->addOption('debug', null, InputOption::VALUE_NONE, 'If set, enter debug mode')
-        ;
-    }
+	protected function configure()
+	{
+		$this
+			->setName('euzeo:fixtures')
+			->setDescription('Load db data from fixtures files')
+			->addArgument('bundle', InputArgument::OPTIONAL, 'The bundle name')
+			->addOption('debug', null, InputOption::VALUE_NONE, 'If set, enter debug mode')
+		;
+	}
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $this->bundle = $input->getArgument('bundle');
-
-        $this->em = $this->getContainer()->get('doctrine')->getEntityManager('default');
-
-        $kernel = $this->getContainer()->get('kernel');
-		$path = $kernel->locateResource('@'.$this->bundle.'/Data/fixtures/');
-
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		// init
+		$this->bundle = $input->getArgument('bundle');
+		$this->em = $this->getContainer()->get('doctrine')->getEntityManager('default');        
+		$this->entities = array();
 		$this->isDebug = TRUE === $input->getOption('debug');
+		
+
+		$kernel = $this->getContainer()->get('kernel');
+		$path = $kernel->locateResource('@'.$this->bundle.'/Data/fixtures/');		
 
 		if ($this->isDebug) {
 			$output->writeln('Reading yml files from folder '.$path);
 		}
 
-        if ($handle = opendir($path))
-        {
-		    while (false !== ($entry = readdir($handle)))
-		    {
-		        if ( substr($entry, -4) === '.yml' )
-		        {
-		        	if ($this->isDebug) $output->writeln('Found '.$entry);
+		if ($handle = opendir($path)) {
+			// loop through each files in the fixtures folder
+			while (false !== ($entry = readdir($handle)))
+			{
+				if ( substr($entry, -4) === '.yml' ) {
+					if ($this->isDebug) $output->writeln('Found '.$entry);
 
-		        	$this->loadFixtureFrom($path.$entry);
-		        }
-		    }
+					$this->loadFixtureFrom($path.$entry);
+				}
+			}
 
-		    closedir($handle);
+			closedir($handle);
 		}
-    }
+	}
 
-    protected function loadFixtureFrom($file)
-    {
-    	$this->fixtures  = Yaml::parse(file_get_contents($file));
-
-    	// in this array we'll store all entity object
-    	$entities = array();
+	protected function loadFixtureFrom($file)
+	{
+		$this->fixtures  = Yaml::parse(file_get_contents($file));
 		
-    	foreach ($this->fixtures as $model => $data)
-    	{
-    		$repos = $this->em->getRepository($this->bundle.':'.$model);
+		// loop though each different entity type 
+		foreach ($this->fixtures as $model => $data)
+		{
+			$repos = $this->em->getRepository($this->bundle.':'.$model);
 
-    		$className = $repos->getClassName();
-    		$entity = new $className;
+			$className = $repos->getClassName();
+			
+			// initialize the array for the current model
+			$this->entities[$model] = array();
 
-    		$entities[$model] = array();
+			// loop through each entity object 
+			foreach ($data as $objectName => $field) 
+			{
+				// create the entity object
+				$entity = new $className;
 
-    		foreach ($data as $objectName => $field) 
-    		{
-    			$entity = new $className;
+				// loop through each entity field
+				foreach ($field as $fieldName => $fieldValue)
+				{
+					// format $fieldName to match the property inside the Entity
+					$fieldName = ucwords($fieldName);				
 
-    			foreach ($field as $fieldName => $fieldValue)
-    			{
-    				$fieldName = ucwords($fieldName);
+					if ($this->isComplexType($fieldName)) {
+						// we need to fetch the object first
+						$fieldValue = $this->entities[$fieldName][$fieldValue];    					
+					}
+					
 					$function = 'set'.$fieldName;
+					$entity->$function($fieldValue);
+				}
 
-    				if ($this->isComplexType($fieldName))
-    				{
-    					// we need to fetch the object first
-    					$complexObject = $entities[$fieldName][$fieldValue];
+				//$this->em->persist($entity);
 
-    					$entity->$function($complexObject);
-    				}
-    				else {
-    					$entity->$function($fieldValue);
-    				}
-    			}
+				$this->entities[$model][$objectName] = $entity;
+			}
+			
+			//$this->em->flush();
+		}
 
-    			//$this->em->persist($entity);
-				//$this->em->flush();
+		if ($this->isDebug) {
+			var_dump($entities);
+		}
+	}
 
-				$entities[$model][$objectName] = $entity;
-    		}
-    	}
+	protected function isComplexType($fieldName)
+	{
+		// TODO: reflect the field inside the entity to know the exact type
 
-    	if ($this->isDebug) {
-    		var_dump($entities);
-    	}
-    }
-
-    protected function isComplexType($fieldName)
-    {
-    	// TODO: reflect the field inside the entity to know the exact type
-
-    	return array_key_exists($fieldName, $this->fixtures);
-    }
+		return array_key_exists($fieldName, $this->fixtures);
+	}
 }
